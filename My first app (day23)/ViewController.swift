c2826317660c2f983ce56d0aea4c4c7b2250fda3
Roman Gorodilov <panda.pleasant@gmail.com>
2021-04-30 08:17:40 +0400

    Initial Commit

---
+++ My first app (day23)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My first app (day23)
+//
+//  Created by Roman Gorodilov on 30.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
